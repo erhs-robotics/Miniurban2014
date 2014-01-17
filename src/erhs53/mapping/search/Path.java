274ad1b02fabd1cb1fa51aa27bb5dba84ebf8b3b
Michael Stevens <mls0.5mv2@gmail.com>
2014-01-16 22:46:00 -0500

    adding comments

--- src/erhs53/mapping/search/Path.java
+++ src/erhs53/mapping/search/Path.java
@@ -2,13 +2,20 @@ package erhs53.mapping.search;
 
 import java.util.ArrayList;
 
+/**
+ * Instances of Path provide convenience functions to an array of actions.
+ * Path also provides a Cost First Search method statically.
+ * @author Michael Stevens
+ *
+ */
 public class Path {
-	public ArrayList<Action> actions;
-	public double cost;
+	public ArrayList<Action> actions;/** The actions that make up the path*/
+	public double cost;/**The cost of following the path*/
 
 	public Path() {
 		actions = new ArrayList<>();
 	}
+	
 
 	public Path(Action a, double cost) {
 		actions = new ArrayList<Action>();
@@ -22,6 +29,11 @@ public class Path {
 		this.cost = cost;
 	}
 
+	/**
+	 * Returns the ith action in the path
+	 * @param i index
+	 * @return the ith action
+	 */
 	public Action get(int i) {
 		return actions.get(i);
 	}
@@ -38,15 +50,24 @@ public class Path {
 		actions.add(a);
 	}
 
-	public boolean contains(State state) {
+	/**
+	 * Performs a linear search for a state visited in the path
+	 * @param state the state to search for
+	 * @return if the state was found or not
+	 */
+	private boolean contains(State state) {
 		for (Action a : actions) {
 			if (a.state.name.equals(state.name))
 				return true;
 		}
 		return false;
 	}
-
-	public ArrayList<Path> successors() {
+	
+	/**
+	 * Returns a list of all possible paths proceeding one step forward in all directions
+	 * @return
+	 */
+	private ArrayList<Path> successors() {
 		ArrayList<Path> paths = new ArrayList<>();
 		for (Action a : lastState().actions(this)) {
 			if (!this.contains(a.state)) {
@@ -66,6 +87,11 @@ public class Path {
 		return actions.get(actions.size() - 1).state;
 	}
 
+	/**
+	 * Returns the path of minimum cost
+	 * @param paths
+	 * @return
+	 */
 	private static Path min(ArrayList<Path> paths) {
 		Path best = paths.get(0);
 		for (Path p : paths) {
@@ -112,6 +138,12 @@ public class Path {
 		}
 	}
 
+	/**
+	 * Performs a cost first search
+	 * @param start The start position
+	 * @param end The end position
+	 * @return A Path containing the list of actions needed to move from state start to state end
+	 */
 	public static Path CFS(State start, State end) {
 		ArrayList<Path> open = new ArrayList<>();
 		open.add(new Path(new Action(0, start), 0));
--- src/erhs53/mapping/search/State.java
+++ src/erhs53/mapping/search/State.java
@@ -2,6 +2,11 @@ package erhs53.mapping.search;
 
 import java.util.ArrayList;
 
+/**
+ * Used to define the needed fields and methods for a State
+ * @author Michael Stevens
+ *
+ */
 public abstract class State {
 	public String name;
 	public double cost;
