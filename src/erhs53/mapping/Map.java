cdb8c8c7d503a1c7879d150b6c597f315ae36767
davidofthedragons <davidofthedragons@gmail.com>
2014-01-23 17:56:08 -0500

    Starting map

--- src/erhs53/mapping/Map.java
+++ src/erhs53/mapping/Map.java
@@ -30,11 +30,84 @@ public class Map {
 
 	/** ROAD LIST *****************************************************************/
 	// Every road in the map
-	public static Road V1 = new Road("V1", 1), V2 = new Road("V2", 1),
+	/*public static Road V1 = new Road("V1", 1), V2 = new Road("V2", 1),
 			V3 = new Road("V3", 1), V4 = new Road("V4", 1), V5 = new Road("V5", 2),
 			H1 = new Road("H1", 2), H2 = new Road("H2", 1), H3 = new Road("H3", 1), 
-			H4 = new Road("H4", 1), H5 = new Road("H5", 1);
-	
+			H4 = new Road("H4", 1), H5 = new Road("H5", 1);*/
+	public static Road ENTRY = new Road("ENTRY", 0),
+					   AV0 = new Road("AV0", 285),
+					   AV1 = new Road("AV1", 100),
+					   AV2 = new Road("AV2", 80),
+					   AV3 = new Road("AV3", 60),
+					   AV4 = new Road("AV4", 70),
+					   AV5 = new Road("AV5", 50),
+					   AV6 = new Road("AV6", 90),
+					   AV7 = new Road("AV7", 70),
+					   AV8 = new Road("AV8", 75),
+					   AH0 = new Road("AH0", 210),
+					   AH1 = new Road("AH1", 260),
+					   AH2 = new Road("AH2", 180),
+					   AH3 = new Road("AH3", 110),
+					   AH4 = new Road("AH4", 265),
+					   AH5 = new Road("AH5", 150),
+					   AH7 = new Road("AH7", 120),
+					   AH8 = new Road("AH8", 310),
+					   AC0 = new Road("AC0", 85),
+					   AC1 = new Road("AC1", 125),
+					   AC2 = new Road("AC2", 80),
+					  AC3B = new Road("AC3B", 75),
+							 
+					   BV0 = new Road("BV0", 50),
+					   BV1 = new Road("BV1", 50),
+					   BV2 = new Road("BV2", 50),
+					   BV3 = new Road("BV3", 50),
+					   BV4 = new Road("BV4", 90),
+					   BV5 = new Road("BV5", 50),
+					   BV6 = new Road("BV6", 90),
+					   BV7 = new Road("BV7", 75),
+					   BV8 = new Road("BV8", 20),
+					   BV9 = new Road("BV9", 75),
+					   BV10 = new Road("BV10", 75),
+					   BV11 = new Road("BV11", 75),
+					   BV12 = new Road("BV12", 75),
+					   BV13 = new Road("BV13", 75),
+					   BH0 = new Road("BH0", 210),
+					   BH1 = new Road("BH1", 150),
+					   BH2 = new Road("BH2", 105),
+					   BH3 = new Road("BH3", 110),
+					   BH4 = new Road("BH4", 150),
+					   BH5 = new Road("BH5", 110),
+					   BH6 = new Road("BH6", 65),
+					   BH7 = new Road("BH7", 110),
+					   BH8 = new Road("BH8", 105),
+					   BH9 = new Road("BH9", 110),
+					   BH10 = new Road("BH10", 150),
+					   BH11 = new Road("BH11", 180),
+					   BH12 = new Road("BH12", 165),
+							 
+					   CV0 = new Road("CV0", 50),
+					   CV1 = new Road("CV1", 50),
+					   CV2 = new Road("CV2", 210),
+					   CV3 = new Road("CV3", 165),
+					   CV4 = new Road("CV4", 165),
+					   CV5 = new Road("CV5", 75),
+					   CV7 = new Road("CV7", 75),
+					   CV8 = new Road("CV8", 75),
+					   CV9 = new Road("CV9", 75),
+					   CV10 = new Road("CV10", 75),
+					   CH0 = new Road("CH0", 160),
+					   CH1 = new Road("CH1", 205),
+					   CH2 = new Road("CH2", 160),
+					   CH3 = new Road("CH3", 115),
+					   CH4 = new Road("CH4", 90),
+					   CH5 = new Road("CH5", 70),
+					   CH6 = new Road("CH6", 90),
+					   CH7 = new Road("CH7", 90),
+					   CH8 = new Road("CH8", 115),
+					   CH9 = new Road("CH9", 90),
+					   CH11 = new Road("CH11", 180),
+					   CH12 = new Road("CH12", 90);
+							 
 	/*****************************************************************************/
 	
 	/** GOAL LIST *****************************************************************/
@@ -46,7 +119,7 @@ public class Map {
 	/** MAP Generator ************************************************************/
 	// Sets the actions available in each road
 	static {
-		V1.setActions(a(TR, H1));
+		/*V1.setActions(a(TR, H1));
 		V2.setActions(a(TL, H2));
 		V3.setActions(a(TR, H4));
 		V4.setActions(a(GS, V3));
@@ -62,7 +135,36 @@ public class Map {
 		G2 = new Goal("G2", Map.H4, 0);
 		G3 = new Goal("G3", Map.V5, 0); 
 		START = new Goal("START", Map.V2, 0);
-		END = new Goal("END", Map.V2, 0);
+		END = new Goal("END", Map.V2, 0);*/
+		
+		//Section A
+		AH0.setActions(a(TL, AV1));
+		AH1.setActions(a(TL, AV1), a(GS, AH0));
+		AH2.setActions(a(GS, BH2), a(TR, AV6));
+		AH3.setActions(a(TL, AV4));
+		AH4.setActions(a(GS, BH8), a(TR, AV8));
+		AH5.setActions(a(GS, AH7), a(TL, AC2));
+		AH7.setActions(a(GS, AH8));
+		AH8.setActions(a(TL, BV10), a(GS, BH11));
+		AV0.setActions(a(TL, AH5));
+		AV1.setActions(a(GS, AV2), a(TL, AC1));
+		AV2.setActions(a(TL, AH4), a(GS, AV3));
+		AV3.setActions(a(TL, AC3B));
+		AV4.setActions(a(TL, AH4));
+		AV5.setActions(a(GS, AV6), a(TL, BH2));
+		AV7.setActions(a(GS, AV8), a(TL, BH8));
+		AV8.setActions(a(TL, AH8));
+		AC0.setActions(a(GS, AV1));
+		AC1.setActions(a(GS, AC0), a(TR, AH2));
+		AC2.setActions(a(GS, AC3B));
+		AC3B.setActions(a(TL, AH8));
+		//Section B
+		BH0.setActions(a(TL, AV5), a(GS, AH1));
+		BH1.setActions(a(GS, BH0), a(TL, BV1));
+		BH2.setActions(a(TL, BV0), a(GS, BH3));
+		BH3.setActions(a(TR, BV5), a(GS, BH4));
+		
+		
 		
 	}
 	
